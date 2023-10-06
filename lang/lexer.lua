ca8fa37cc3497879590cef7fcf7e769026b81578
Ron Tseytlin <ronts390@gmail.com>
2023-10-06 06:37:19 +0300

    added lexer

---
+++ lang/lexer.lua
@@ -0,0 +1,75 @@
+---@class Lexer
+---@field code string
+---@field index number
+---@field finished boolean
+local lexer = {}
+lexer.__index = lexer
+
+function lexer.new(code)
+  return setmetatable({
+    code = code,
+    index = 1,
+    finished = false,
+  }, lexer)
+end
+
+function lexer:advance(count)
+  self.index = self.index + (count or 1)
+  self.finished = self.index > #self.code
+end
+
+---@return string
+function lexer:charAt(i)
+  return self.code:sub(i, i)
+end
+
+---@return string
+function lexer:curChar()
+  return self:charAt(self.index)
+end
+
+function lexer:nextToken()
+  -- skip whitespace and comments
+  while self:curChar():match("[%s%;]") and not self.finished do
+    if self:curChar() == ";" then
+      while self:curChar() ~= "\n" and not self.finished do
+        self:advance()
+      end
+    end
+    self:advance()
+  end
+
+  if self.finished then
+    return { type = "EOF" }
+  end
+
+  if self:curChar() == "(" then
+    self:advance()
+    return { type = "lparen" }
+  end
+
+  if self:curChar() == ")" then
+    self:advance()
+    return { type = "rparen" }
+  end
+
+  if self:curChar():match("%d") or (self:curChar() == "-" and self:charAt(self.index + 1):match("%d")) then
+    local start = self.index
+    if self:curChar() == "-" then
+      self:advance()
+    end
+    while self:curChar():match("[%d%.]") do
+      self:advance()
+    end
+    return { type = "number", value = tonumber(self.code:sub(start, self.index - 1)) }
+  end
+
+  -- any other token
+  local start = self.index
+  while self:curChar():match("[^%s%;%(%)]") and not self.finished do
+    self:advance()
+  end
+  return { type = "name", value = self.code:sub(start, self.index - 1) }
+end
+
+return lexer
